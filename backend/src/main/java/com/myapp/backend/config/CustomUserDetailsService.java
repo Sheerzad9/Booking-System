f916f84313ce8070a007b9d733151eabd40daf31
Ali Sheerzad <ali.sheerzad9@gmail.com>
2024-01-07 13:10:10 +0200

    CustomUserDetailsService to fetch our user roles

--- backend/src/main/java/com/myapp/backend/config/CustomUserDetailsService.java
+++ backend/src/main/java/com/myapp/backend/config/CustomUserDetailsService.java
@@ -1,2 +1,34 @@
-package com.myapp.backend.config;public class CustomUserDetailsService {
+package com.myapp.backend.config;
+
+import com.myapp.backend.dao.UserDao;
+import com.myapp.backend.model.Role;
+import com.myapp.backend.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userDao.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toList());
+    }
 }
