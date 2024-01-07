a6c105c91b5efce5581ed95edb4ad06bd18124a4
Ali Sheerzad <ali.sheerzad9@gmail.com>
2024-01-07 13:14:07 +0200

    Dto class for register requesy

--- backend/src/main/java/com/myapp/backend/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/myapp/backend/dto/request/RegisterRequest.java
@@ -1,2 +1,19 @@
-package com.myapp.backend.dto.request;public class RegisterRequest {
+package com.myapp.backend.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + " lastName: " + lastName + " email: " + email + " password: " + password;
+    }
 }
